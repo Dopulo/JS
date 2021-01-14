908262e48170c1e6467d96bfc0a7335116e7f6bd
Alex <lubik_lovelady@me.com>
2021-01-14 21:40:03 +0300

    first

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше фамилия?', '');
+// answers[1] = prompt('Какая у вас имя?', '');
+// answers[2] = prompt('Как ваше отчество?', '');
+
+// console.log(typeof(answers));
+
+// ИНТЕРПОЛЯЦИЯ!!!
+
+// const category = 'toys';
+
+// console.log(`/https://someurl.com/${category}/5/6`);
+
+// const user = "Ivan";
+
+// alert(`ПИДАРАС ТЫ, ${answers}`);
+
+//
