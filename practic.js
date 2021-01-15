74c2245c23f7466ab5fb6530b905a31744fd2902
Dopulo <lubik_lovelady@me.com>
2021-01-15 23:25:14 +0300

    if & else

--- practic.js
+++ practic.js
@@ -1,19 +1,2 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+"use strict";
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    ganres: [],
-    private: false
-};
-
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
--- script.js
+++ script.js
@@ -25,3 +25,59 @@
 
 // alert(`ПИДАРАС ТЫ, ${answers}`);
 
+// const numberOfFilms = prompt('сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// if (4 == 9) {
+//     console.log('ok!');
+// } else {
+//     console.log('Error');
+// }
+
+const answers = [];
+
+answers[0] = prompt('Ваше имя?', '');
+answers[1] = prompt('Ваша фамилия?', '');
+
+const name = "николай";
+
+if (answers[0] == name) {
+    alert(`ПИДАРАС ТЫ, ${answers[0]}`);
+} else if (answers[0] !== name) {
+alert(`Привет, ${answers[1]} ${answers[0]}`);
+}
+
+(answers[0] == name) ? alert(`ПИДАРАС ТЫ, ${answers[0]}`) : alert(`Привет, ${answers[1]} ${answers[0]}`);
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case 50:
+//         console.log('В точку!');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
